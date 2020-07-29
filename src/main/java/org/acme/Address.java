51898f735aadc84a1518d472d120964c660d0e26
FRAD <FRAD@examen.com.pe>
2020-07-29 11:59:37 -0500

    First Commit

---
+++ src/main/java/org/acme/Address.java
@@ -0,0 +1,40 @@
+package org.acme;
+
+public class Address {
+    private int id;
+    private String city;
+    private String avenue;
+    private int number;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAvenue() {
+        return avenue;
+    }
+
+    public void setAvenue(String avenue) {
+        this.avenue = avenue;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
---
+++ src/main/java/org/acme/ExampleResource.java
@@ -0,0 +1,29 @@
+package org.acme;
+import javax.print.attribute.standard.Media;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+@Path("/mis-datos")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ExampleResource {
+    private Set<Person> persons= Collections.newSetFromMap(Collections.synchronizedMap(new LinkedHashMap<>()));
+    public ExampleResource(){
+        persons.add(new Person(0,"Fernando","Arevalo",20));
+        persons.add(new Person(1,"Juan","Perez",20));
+        for (Person person: persons) {
+            System.out.println(person);
+        }
+    }
+
+    @GET
+    public Set<Person> hello() {
+        return persons;
+    }
+}
